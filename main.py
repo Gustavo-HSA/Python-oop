74dba8e8e711e8696e785a28a2b7e0ab278bedb2
Gustavo <xdrules1995@gmail.com>
2022-04-26 13:30:28 -0300

    new classes

--- main.py
+++ main.py
@@ -1,17 +1,36 @@
 class Item:
-    def __init__(self, name: str, price: int, quantity=0):
+    pay_rate = 0.8  # The pay rate after 20% discount
+    all = []
+
+    def __init__(self, name: str, price: float, quantity=0):
+        # Run validations to the received arguments
+        assert price >= 0, f"Price {price} is not greater than or equal to zero!"
+        assert quantity >= 0, f"Quantity {quantity} is not greater than or equal to zero!"
+
+        # Assign to self object
         self.name = name
         self.price = price
         self.quantity = quantity
 
-    
+        # Actions to execute
+        Item.all.append(self)
+
     def calculate_total_price(self):
         return self.price * self.quantity
 
+    def apply_discount(self):
+        self.price = self.price * self.pay_rate
 
-item1 = Item(1, 100, 1)
+    def __repr__(self):
+        return f"Item('{self.name}', {self.price}, {self.quantity})"
 
+
+item1 = Item("Phone", 100, 1)
 item2 = Item("Notebook", 1000, 3)
+item3 = Item("Cable", 10, 5)
+item4 = Item("Mouse", 50, 5)
+item5 = Item("Keyboard", 75, 5)
 
-print(item1.calculate_total_price())
-print(item2.calculate_total_price())
+print(Item.all)
+# print(Item.__dict__)  # All the attributes for Class level
+# print(item1.__dict__)  # All the attributes for instance level
